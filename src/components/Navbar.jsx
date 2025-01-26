885f1b7cfdca5d97e4ef5966dcbed8fdc73ec655
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-01-26 18:37:31 +0530

    added best Seller component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium  w-full max-w-7xl mx-auto'>
 
-      <img src={assets.logo} className='w-36' alt = "" />
+     <Link to='/'> <img src={assets.logo} className='w-36' alt = "" /> </Link>
 
      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
        

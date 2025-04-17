59d95679ae7d51a41d85e04aaa865ade930835aa
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-17 17:02:05 +0530

    admin panel login feature completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
@@ -6,16 +6,23 @@ import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+ useEffect(() => {
+  localStorage.setItem('token', token)
+ }, [token])
+
   return (
     <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
       {token === ""
-        ? <Login />
+        ? <Login  setToken={setToken} />
         : <>
           <Navbar />
           <hr className="border-t border-gray-300" />

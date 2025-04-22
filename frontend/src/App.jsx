3911589bb2945b376e598adec9574bd128e26027
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-22 10:31:52 +0530

    moved preloader from home page to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import {Routes, Route} from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -14,10 +15,26 @@ import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/verify'
+import Verify from './pages/verify';
+import Preloader from './components/Preloader'
 
 
 const App = () => {
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1200) // show preloader for 1.2s (or less if you prefer)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return <Preloader />
+  }
+
   return (
     <div className = 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
    <ToastContainer />

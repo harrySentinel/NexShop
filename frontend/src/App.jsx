6cc0ae026a48c1e8a6cb3c12a71f5cf2acc87fad
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-22 12:30:03 +0530

    resolve verify -> Verify import issue in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/verify';
+import Verify from './pages/Verify';
 import Preloader from './components/Preloader'
 
 
